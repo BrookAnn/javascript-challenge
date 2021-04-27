e498db389caef1195bf42cfd610a7395f7da9d3b
Brookann <blaire08@ymail.com>
2021-04-27 16:41:35 -0500

    Finished

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,49 @@
-// from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+
+
+console.log(data);
+
+data.forEach(function (tabledata) {
+
+    var row = tbody.append("tr");
+    Object.values(tabledata).forEach(function (value) {
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+var filterButton = d3.select("#filter-btn");
+
+
+function handleChange() {
+    var inputText = d3.select("#datetime")
+    var inputvalue = inputText.property("value");
+
+    console.log(inputvalue);
+    var filtered = data.filter(row => {
+        return row.datetime == inputvalue;
+    });
+    console.log(filtered)
+    var tbody = d3.select("tbody");
+    var table = d3.select("table");
+    tbody.remove();
+    tbody = table.append('tbody');
+    filtered.forEach(function (tabledata) {
+        console.log(tabledata)
+        var row = tbody.append("tr");
+        Object.values(tabledata).forEach(function (value) {
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+filterButton.on("click", handleChange);
+var ID =d3.select('ID')
+ID.on('enter',handleChange);
